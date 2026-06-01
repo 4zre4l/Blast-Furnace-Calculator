6bdb10f38649cdb07d49d28ee190be1ba1cfaf3a
Ciaran <CiaranJEastwood@gmail.com>
2026-06-01 07:10:23 +0100

    little stretch cuh

--- Front End.py
+++ Front End.py
@@ -6,7 +6,7 @@ from io import BytesIO
 ids = [453,440,444,447,449,451]
 
 root = tk.Tk()
-root.geometry("300x300")
+root.geometry("500x300")
 
 class Display_Item(tk.Frame):
     def __init__(self,parent,item_id):
@@ -20,16 +20,17 @@ class Display_Item(tk.Frame):
         self.img = tk.Label(self,image = self.photo)
         self.price = tk.Label(self,text = Back.fetch_item_price(self.item_id))
 
-        self.name.grid(row=1, column=1)
-        self.img.grid(row = 2,column = 1)
-        self.price.grid(row = 3,column = 1)
+        self.name.grid(row=1, column=1,sticky = "ew")
+        self.img.grid(row = 2,column = 1,sticky = "ew")
+        self.price.grid(row = 3,column = 1,sticky = "ew")
 
 
 
 counter=1
 for id in ids:
     id = Display_Item(root,id)
-    id.grid(row = 1,column = counter)
+    id.grid(row = 1,column = counter,padx = 10,pady = 10)
+    root.columnconfigure(counter,weight = 1)
     counter+=1
 
 root.mainloop()
